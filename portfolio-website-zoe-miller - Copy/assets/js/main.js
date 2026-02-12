5958c8b30e43c08eb440b89e809713349fd0897f
Katartizo <103197522+Katartizo@users.noreply.github.com>
2026-02-12 12:58:06 +0100

    Update main.js

--- portfolio-website-zoe-miller - Copy/assets/js/main.js
+++ portfolio-website-zoe-miller - Copy/assets/js/main.js
@@ -240,3 +240,14 @@ if(themeButton){
         localStorage.setItem('selected-icon', getCurrentIcon());
     });
 }
+
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
